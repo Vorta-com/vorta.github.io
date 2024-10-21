dd27a3a14ce75711a82adf162eeab31bba926e84
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-10-21 11:05:59 +0530

    Add files via upload

---
+++ src/config/nav.js
@@ -0,0 +1,28 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About us',
+		slug: '/company/about',
+	},
+	{
+		title: 'GOM',
+		slug: '/product/product-2',
+	},
+	{
+		title: 'VBot',
+		slug: '/product/product-1',
+	},
+	{
+		title: 'Blog',
+		slug: '/blog',
+	},
+	{
+		title: 'Contact Us',
+		slug: '/company/contact',
+	},
+
+
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Vorta`,
+	description: `Vorta is a tech-driven startup revolutionizing the way innovative products reach consumers. At the intersection of technology and commerce, Vorta empowers creators and entrepreneurs to bring their groundbreaking ideas to market seamlessly. With a focus on cutting-edge solutions and a commitment to quality, Vorta is your gateway to the next generation of technology, providing tools, platforms, and support to ensure that tomorrow’s innovations are accessible today. Whether you're launching a new gadget or a tech-driven service, Vorta is your partner in turning vision into reality.`,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `Vorta`,
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
